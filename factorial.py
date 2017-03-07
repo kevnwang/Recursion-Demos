45b9ccb087472f9a2d6c7c5998abcfc229b32e49
kwang21093@gmail.com <kwang21093@gmail.com>
2017-03-07 01:51:07 -0500

    upload

---
+++ factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    # print "push: factorial(" + str(n) + ")"
+    if n <= 1:
+        # print "start popping"
+        return n
+    else:
+        product = n * factorial(n-1)
+        # print "pop: " + str(n) + " * factorial(" + str(n - 1) + ") = " + str(product)
+        return product
---
+++ iter_factorial.py
@@ -0,0 +1,5 @@
+def iterative_factorial(n):
+    product = 1
+    for i in range(2,n+1):
+        product *= i
+    return product
